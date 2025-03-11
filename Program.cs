af2dcd963a4de5415010228b572b8674f5cae3bc
Rayo25Gumi <marcguma@pm.me>
2025-03-11 23:30:50 +0100

    frist

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string url = "https://api.wheretheiss.at/v1/satellites/25544";
+        using (var client = new HttpClient())
+        {
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            string jsonResponse = response.Content.ReadAsStringAsync().Result;
+
+            var pokeApiObject = JsonConvert.DeserializeObject<Iss>(jsonResponse);
+
+            double Latitud = issApiObject.Latitud;
+            double Longitud = pokeApiObject.growth_time;
+            Console.WriteLine(name);
+            Console.WriteLine(size);
+        }
+    }
+
+    private class Iss
+    {
+        [JsonProperty("latitude")]
+        public string Latitud { get; set; }
+
+        [JsonProperty("longitude")]
+        public string Longitud { get; set; }
+    }
+}
